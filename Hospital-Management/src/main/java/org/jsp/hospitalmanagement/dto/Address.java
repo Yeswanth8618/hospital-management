213b2fbef696b0a7f5f8b85cf9f0b6993ae70fba
Yeswanth8618 <yeswanth9696@gmail.com>
2024-03-01 12:42:54 +0530

    added

---
+++ Hospital-Management/src/main/java/org/jsp/hospitalmanagement/dto/Address.java
@@ -0,0 +1,84 @@
+package org.jsp.hospitalmanagement.dto;
+
+
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Address {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+@Column(nullable = false)
+private String Street,buildingname,landmark,area,city,state,country;
+@Column(nullable = false)
+private int pincode;
+
+public String getStreet() {
+	return Street;
+}
+public void setStreet(String street) {
+	Street = street;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getBuildingname() {
+	return buildingname;
+}
+public void setBuildingname(String buildingname) {
+	this.buildingname = buildingname;
+}
+public String getLandmark() {
+	return landmark;
+}
+public void setLandmark(String landmark) {
+	this.landmark = landmark;
+}
+public String getArea() {
+	return area;
+}
+public void setArea(String area) {
+	this.area = area;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public String getState() {
+	return state;
+}
+public void setState(String state) {
+	this.state = state;
+}
+public String getCountry() {
+	return country;
+}
+public void setCountry(String country) {
+	this.country = country;
+}
+public int getPincode() {
+	return pincode;
+}
+public void setPincode(int pincode) {
+	this.pincode = pincode;
+}
+
+
+}
